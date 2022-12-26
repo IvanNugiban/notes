f46c9d6caa67268db8a49824ae143c7f17c53eb7
IvanNugiban <aa1245541@gmail.com>
2022-12-26 19:12:53 +0200

    Fixed cors

--- server/middleware/cors.middleware.js
+++ server/middleware/cors.middleware.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://notes-ua.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://notes-dusky-theta.vercel.app/');
     res.header('Access-Control-Allow-Methods', "GET, PUT, PATCH, OPTIONS, POST, DELETE");
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization' );
 
